912d906d9a74798238d06b84ec11cdce38b11b30
antoniodcorrea <hello@antoniodiaz.me>
2020-02-05 11:45:10 +0300

    Add circle loader

---
+++ src/components/LoaderCircle/LoaderCircle.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import LoaderCircle from './LoaderCircle';
+
+storiesOf('LoaderCircle', module).add('Default', () => <LoaderCircle />);
