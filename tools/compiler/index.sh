00489652a912d9264b0ed5b4c1769f7f089c34be
antoniodcorrea <hello@antoniodiaz.me>
2020-02-20 01:01:10 +0300

    Updated compiler

--- tools/compiler/index.sh
+++ tools/compiler/index.sh
@@ -1,6 +1,20 @@
-find "src" -name '*.less' | while read line; do
+# Builder for library
+# Transpile css
+find src -name '*.less' -not -path 'src/css/*' | while read line; do
     REPLACE=`echo $line | sed "s|\.less|\.css|"`
-
-    echo "$line --> $REPLACE"
     (lessc "$line" "$REPLACE" &)
 done
+# Clean previous components folder
+find components -name '*' -delete
+# Compile TS
+tsc -d
+# Replace .less for .css
+find components -type f \( -name "*.js" -o -name "*.d.ts" \) -exec sed -i '' 's#\.less'\''#\.css'\''#' {} ';'
+cd src
+# Remove css comments
+find . -name '*.css'  -exec sed -i '' '/\/\*.*\*\//d;/\/\*/,/\*\// d' {} ';'
+# Copy css and svgs
+find .  -type f \( -name "*.css" -o -name "*.svg" \) -not -path './css/*' -exec rsync -R {} ../../components ';'
+# Clean css
+find . -name '*css' -delete
+echo "Compiled success"
