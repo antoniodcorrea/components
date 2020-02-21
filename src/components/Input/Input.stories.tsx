5ec9b088fddce8baaf1664654af7c84f7aa6f984
antoniodcorrea <hello@antoniodiaz.me>
2020-02-22 00:01:54 +0300

    Modify Input style

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -23,6 +23,8 @@ const knobs = {
 
 export const Empty = () => {
   const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+  const [value3, setValue3] = useState('');
 
   return (
     <div style={{ width: '300px' }}>
@@ -30,13 +32,33 @@ export const Empty = () => {
       <Hr type="spacer" />
       <Input
         name={knobs.name()}
-        label={knobs.label()}
+        label="First data"
         value={value1}
         readOnly={knobs.readOnly()}
         error={knobs.error()}
         success={knobs.success()}
         disabled={knobs.disabled()}
         onChange={e => setValue1(e.target.value)}
+      />
+      <Input
+        name={knobs.name()}
+        label="Second value"
+        value={value2}
+        readOnly={knobs.readOnly()}
+        error={knobs.error()}
+        success={knobs.success()}
+        disabled={knobs.disabled()}
+        onChange={e => setValue2(e.target.value)}
+      />
+      <Input
+        name={knobs.name()}
+        label="Third value"
+        value={value3}
+        readOnly={knobs.readOnly()}
+        error={knobs.error()}
+        success={knobs.success()}
+        disabled={knobs.disabled()}
+        onChange={e => setValue3(e.target.value)}
       />
       <Hr type="spacer" />
     </div>
