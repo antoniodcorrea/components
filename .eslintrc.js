2b9abefbc3e91d4e0358fb35e8942f94437badab
antoniodcorrea <hello@antoniodiaz.me>
2020-03-22 14:20:35 +0300

    Add Eslint conf

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    indent: ['warn', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'no-console': 0,
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'arrow-parens': ['error', 'always'],
+    'quote-props': ['warn', 'consistent'],
+    'no-duplicate-imports': ['warn', { includeExports: false }],
+    'no-redeclare': 0,
+    'no-dupe-keys': 0,
+    'no-debugger': 0,
+    'newline-before-return': 'error',
+    'no-multi-spaces': 'error',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
