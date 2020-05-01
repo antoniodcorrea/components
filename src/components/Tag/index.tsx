889219e876d9e3a218c376ced663d107ca019166
antoniodcorrea <hello@antoniodiaz.me>
2020-05-01 14:49:54 +0300

    Add Tag ClassName prop

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import './Tag.less';
 import { Span } from '../Span';
 import { Notification } from '../Notification';
+
+import './Tag.less';
 
 export type Notification = 'success' | 'error' | 'alert';
 
 interface Props {
   children: string;
+  className?: string;
   size?: 'small' | 'medium' | 'big';
   variant?: 'alternate';
   notification?: Notification;
 }
 
-export const Tag: React.FC<Props> = ({ children, size = 'small', variant, notification }) => (
-  <div className={'Tag ' + (size ? 'Tag--' + size : '') + (variant ? ' Tag--' + variant : '')}>
+export const Tag: React.FC<Props> = ({ children, size = 'small', variant, notification, className }) => (
+  <div
+    className={
+      'Tag ' + (size ? 'Tag--' + size : '') + (variant ? ' Tag--' + variant : '') + className ? ' ' + className : ''
+    }
+  >
     <Notification className="Tag-notification" type={notification} size="small" />
     <div className="Tag-content">
       <Span size="micro" bold>
