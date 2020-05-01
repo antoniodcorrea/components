6971f38d8a358bd5c0e684d6b6219fe8a72c476e
antoniodcorrea <hello@antoniodiaz.me>
2020-05-01 16:19:01 +0300

    Refactor Tag style

--- src/components/Tag/Tag.stories.tsx
+++ src/components/Tag/Tag.stories.tsx
@@ -15,11 +15,50 @@ const knobs = {
 };
 
 export const Default: React.FC = () => (
-  <>
+  <div style={{ padding: '10px' }}>
     <H1>Tag</H1>
     <Hr type="spacer" />
     <Tag notification={knobs.notification()}>Tag</Tag>
-  </>
+    <Hr type="spacer" />
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Cool</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Yeah</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Rock</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Lalala</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>This</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>That</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Lorem</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Red</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Alert</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Articles</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Foo</Tag>
+      </div>
+      <div style={{ margin: '0 4px 4px 0' }}>
+        <Tag notification={knobs.notification()}>Bar</Tag>
+      </div>
+    </div>
+  </div>
 );
 
 export const Alternate: React.FC = () => {
--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -17,14 +17,12 @@ interface Props {
 export const Tag: React.FC<Props> = ({ children, size = 'small', variant, notification, className }) => (
   <div
     className={
-      'Tag ' + (size ? 'Tag--' + size : '') + (variant ? ' Tag--' + variant : '') + className ? ' ' + className : ''
+      'Tag ' + (size ? 'Tag--' + size : '') + (variant ? ' Tag--' + variant : '') + (className ? ' ' + className : '')
     }
   >
     <Notification className="Tag-notification" type={notification} size="small" />
     <div className="Tag-content">
-      <Span size="micro" bold>
-        {children}
-      </Span>
+      <Span bold>{children}</Span>
     </div>
   </div>
 );
