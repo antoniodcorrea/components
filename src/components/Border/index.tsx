13a904a76e96c68a1d725ae8bfe8cb4519f1bd0a
antoniodcorrea <hello@antoniodiaz.me>
2020-10-22 10:20:33 +0300

    Add Border component

--- src/components/Border/index.tsx
+++ src/components/Border/index.tsx
@@ -6,16 +6,20 @@ interface Props {
   padding?: 'small' | 'normal' | 'big';
   className?: string;
   grow?: boolean;
+  onClick?: (any) => void;
+  onMouseLeave?: (any) => void;
 }
 
-export const Border: React.FC<Props> = ({ children, padding = 'normal', grow, className }) => (
+export const Border: React.FC<Props> = ({ children, padding = 'normal', grow, className, onClick, onMouseLeave }) => (
   <div
     className={
+      (className ? className + ' ' : '') +
       'Border' +
       (padding ? ' Border-' + padding : '') +
-      (grow ? ' Border--grow' : '') +
-      (className ? ' ' + className : '')
+      (grow ? ' Border--grow' : '')
     }
+    onClick={onClick}
+    onMouseLeave={onMouseLeave}
   >
     {children}
   </div>
