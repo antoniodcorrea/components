789cdcf3ed0901e828eec4bc421bdb3eacaa4efc
antoniodcorrea <hello@antoniodiaz.me>
2021-01-08 00:06:32 +0300

    Remove forwardref and add id

--- src/components/Border/index.tsx
+++ src/components/Border/index.tsx
@@ -2,32 +2,38 @@ import React from 'react';
 import './Border.less';
 
 interface Props {
+  id?: string;
+  className?: string;
   children: React.ReactNode | React.ReactNode[];
   weight?: 'thick' | 'thin';
   padding?: 'small' | 'normal' | 'big';
-  className?: string;
   grow?: boolean;
   onClick?: (any) => void;
   onMouseLeave?: (any) => void;
 }
 
-export const Border = React.forwardRef<HTMLDivElement, Props>(
-  ({ children, weight = 'thin', padding = 'normal', grow, className, onClick, onMouseLeave }, ref) => (
-    <div
-      ref={ref}
-      className={
-        (className ? className + ' ' : '') +
-        'Border' +
-        (padding ? ' Border-padding--' + padding : '') +
-        (' Border--' + weight) +
-        (grow ? ' Border--grow' : '')
-      }
-      onClick={onClick}
-      onMouseLeave={onMouseLeave}
-    >
-      {children}
-    </div>
-  )
+export const Border: React.FC<Props> = ({
+  id,
+  children,
+  weight = 'thin',
+  padding = 'normal',
+  grow,
+  className,
+  onClick,
+  onMouseLeave,
+}) => (
+  <div
+    id={id}
+    className={
+      (className ? className + ' ' : '') +
+      'Border' +
+      (padding ? ' Border-padding--' + padding : '') +
+      (' Border--' + weight) +
+      (grow ? ' Border--grow' : '')
+    }
+    onClick={onClick}
+    onMouseLeave={onMouseLeave}
+  >
+    {children}
+  </div>
 );
-
-Border.displayName = 'Border';
