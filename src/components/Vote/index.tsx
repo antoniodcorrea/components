520bf003cd33540a95ae2bd71479ce612a3e8d5a
antoniodcorrea <hello@antoniodiaz.me>
2021-01-13 01:49:02 +0300

    Modify vote undefined to null

--- src/components/Vote/index.tsx
+++ src/components/Vote/index.tsx
@@ -13,11 +13,11 @@ export const Vote: React.FC<Props> = ({ vote, changeVote, className }) => (
   <button className={'Vote' + (className ? ' ' + className : '')}>
     <Triangle
       className={'Vote-icon Vote-minus' + (vote === false ? ' Vote--active' : '')}
-      onClick={() => changeVote(vote === false ? undefined : false)}
+      onClick={() => changeVote(vote === false ? null : false)}
     />
     <Triangle
       className={'Vote-icon Vote-plus' + (vote === true ? ' Vote--active' : '')}
-      onClick={() => changeVote(vote === true ? undefined : true)}
+      onClick={() => changeVote(vote === true ? null : true)}
     />
   </button>
 );
