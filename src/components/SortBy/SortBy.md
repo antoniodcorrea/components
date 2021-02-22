8ac3611e62d0b1d56e13f8a1aa10434aac70555f
antoniodcorrea <hello@antoniodiaz.me>
2021-02-22 16:33:09 +0300

    Remove sortby

--- src/components/SortBy/SortBy.md
+++
@@ -1,6 +0,0 @@
-# SortBy component
-
-- Controlled component
-- Requires a full URL
-- Requires options
-- Requires current "activeSort" in form of "option" | "-option"
--- src/components/index.ts
+++ src/components/index.ts
@@ -55,4 +55,3 @@ export { Tooltip } from './Tooltip';
 export { Ul } from './Ul';
 export { SkeletonItem } from './SkeletonItem';
 export { AIcon } from './AIcon';
-export { SortBy } from './SortBy';
