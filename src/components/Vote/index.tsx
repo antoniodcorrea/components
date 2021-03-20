779f1eb6b039b8453d38ff822cf56735ca4506e3
antoniodcorrea <hello@antoniodiaz.me>
2021-03-20 14:20:38 +0300

    Vote add colors

--- src/components/Vote/index.tsx
+++ src/components/Vote/index.tsx
@@ -13,11 +13,11 @@ interface Props {
 export const Vote: React.FC<Props> = ({ vote, changeVote, className, loading = false }) => (
   <button className={'Vote' + (className ? ' ' + className : '') + (loading ? ' Vote--loading' : '')}>
     <Triangle
-      className={'Vote-icon Vote-minus' + (vote === false ? ' Vote--active' : '')}
+      className={'Vote-icon Vote-minus' + (vote === false ? ' Vote-minus--active' : '')}
       onClick={() => changeVote(vote === false ? null : false)}
     />
     <Triangle
-      className={'Vote-icon Vote-plus' + (vote === true ? ' Vote--active' : '')}
+      className={'Vote-icon Vote-plus' + (vote === true ? ' Vote-plus--active' : '')}
       onClick={() => changeVote(vote === true ? null : true)}
     />
   </button>
