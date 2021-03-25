84bbdcb7c300b931aa07810792a18a7355ff5a8b
antoniodcorrea <hello@antoniodiaz.me>
2021-03-26 01:52:20 +0300

    Ellipsis add className

--- src/components/Ellipsis/index.tsx
+++ src/components/Ellipsis/index.tsx
@@ -4,11 +4,12 @@ import React from 'react';
 import './Ellipsis.less';
 
 interface Props {
+  className?: string;
   size?: SpanSize;
 }
 
-export const Ellipsis: React.FC<Props> = ({ size }) => (
-  <Span className="Ellipsis" size={size}>
+export const Ellipsis: React.FC<Props> = ({ className, size }) => (
+  <Span className={'Ellipsis' + (className ? ` ${className}` : '')} size={size}>
     <span className="Ellipsis-dot1">.</span>
     <span className="Ellipsis-dot2">.</span>
     <span className="Ellipsis-dot3">.</span>
