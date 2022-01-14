4c28215009107764f541ece2c06a400e0666f218
antoniodcorrea <hello@antoniodiaz.me>
2022-01-14 17:01:28 +0300

    CarouselField add Image type

--- src/components/index.ts
+++ src/components/index.ts
@@ -74,4 +74,4 @@ export { AIcon } from './AIcon';
 export { SortBy } from './SortBy';
 export { Ellipsis } from './Ellipsis';
 export { Space } from './Space';
-export { CarouselField } from './CarouselField';
+export { CarouselField, CarouselFieldImage } from './CarouselField';
