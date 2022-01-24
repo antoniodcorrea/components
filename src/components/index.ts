4330605d0e4a6b20881ab025c5f4f2860abe7fd6
antoniodcorrea <hello@antoniodiaz.me>
2022-01-24 13:33:16 +0300

    Update sortable

--- src/components/index.ts
+++ src/components/index.ts
@@ -66,7 +66,6 @@ export { TagsSkeleton } from './Tag/TagsSkeleton';
 export { TextArea } from './TextArea';
 export { TextButton } from './TextButton';
 export { TextEditor, TextEditorValue, textEditorDefaultValue, toHtml } from './TextEditor';
-export { SortableList, SortableItem } from './SortableList';
 export { PopOver } from './PopOver';
 export { Tooltip } from './Tooltip';
 export { Ul } from './Ul';
