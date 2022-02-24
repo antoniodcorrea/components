c1339f0429d5ed3363a46382c1c6c1eb48ce2f17
antoniodcorrea <hello@antoniodiaz.me>
2022-02-24 18:16:40 +0300

    HTMLSanitizer maintain target blank

--- src/components/HtmlSanitizer/index.tsx
+++ src/components/HtmlSanitizer/index.tsx
@@ -5,10 +5,20 @@ interface Props {
   html: string;
 }
 
+// set all elements owning target to target=_blank
+// https://github.com/cure53/DOMPurify/issues/317#issuecomment-728100494
+const addTargetHook = (node) => {
+  if ('target' in node) {
+    node.setAttribute('target', '_blank');
+    node.setAttribute('rel', 'noopener');
+  }
+};
+DOMPurify.addHook('afterSanitizeAttributes', addTargetHook);
+
 export const HtmlSanitizer: React.FC<Props> = ({ html }) => {
   if (!html) return null;
 
-  return (
-    <div className="HtmlSanitizer" id="HtmlSanitizer" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html) }} />
-  );
+  const sanitizedHtml = DOMPurify.sanitize(html, { ADD_ATTR: ['target'] });
+
+  return <div className="HtmlSanitizer" id="HtmlSanitizer" dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
 };
