cb62e2f05c22d6dbac14eaaa3e6b6b510ac6db69
antoniodcorrea <hello@antoniodiaz.me>
2022-11-07 17:51:56 +0100

    Load image only on client

--- src/components/Img/index.tsx
+++ src/components/Img/index.tsx
@@ -2,8 +2,6 @@ import React, { ImgHTMLAttributes, useEffect, useState } from 'react';
 
 import './Img.less';
 
-const PLACEHOLDER_IMAGE = 'https://via.placeholder.com/500/ffffff/ffffff';
-
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   className?: string;
   src: string;
@@ -15,7 +13,7 @@ interface Props extends ImgHTMLAttributes<HTMLImageElement> {
 
 export const Img: React.FC<Props> = ({ className, src, sizes, srcSet, title, alt, ...props }) => {
   const [loaded, setLoaded] = useState(false);
-  const [srcInState, setSrcInState] = useState<string>(PLACEHOLDER_IMAGE);
+  const [srcInState, setSrcInState] = useState<string>(null);
 
   const onImageDecode = (img) => {
     if (!img) return;
