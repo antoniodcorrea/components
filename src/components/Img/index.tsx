1403501f25df0f85e5c2d0183f950bbee85f2335
antoniodcorrea <hello@antoniodiaz.me>
2023-05-13 17:44:48 +0200

    Img setLoadFailed false on load

--- src/components/Img/index.tsx
+++ src/components/Img/index.tsx
@@ -26,7 +26,6 @@ export const Img: React.FC<Props> = ({ className, src, sizes, srcSet, title, alt
 
     img.decode().then(() => {
       setLoaded(true);
-      setLoadFailed(false);
 
       if (!srcSet) return;
 
