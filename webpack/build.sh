15167f5ea503cb708f6b7851254d5883b93cbd4c
antoniodcorrea <hello@antoniodiaz.me>
2024-09-20 12:04:03 +0200

    GR-18 Update build process

--- webpack/build.sh
+++ webpack/build.sh
@@ -1,4 +1,7 @@
 ./node_modules/.bin/webpack --config ./webpack/webpack.config.ts
+paste -d "\n" ./dist/src/components/index.d.ts ./dist/src/hooks/index.d.ts > ./dist/index.d.ts_bk
 mv ./dist/src/components/* ./dist
 mv ./dist/src/hooks/* ./dist
+rm ./dist/index.d.ts
+mv ./dist/index.d.ts_bk ./dist/index.d.ts
 rm -rf ./dist/src
